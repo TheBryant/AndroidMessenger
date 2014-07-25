c472762406a161db97acd092bbe9a5a3c3bafabe
Bryant Chang <thebryant@gmail.com>
2014-07-24 20:03:01 -0700

    Rebuilt project to fix some namespace issues

---
+++ app/src/main/java/com/unknottedb/messenger/ListUsersActivity.java
@@ -0,0 +1,82 @@
+package com.unknottedb.messenger;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListUsersActivity extends Activity {
+    private Button logoutButton;
+    private ListView usersListView;
+    private String currentUserId;
+    private ArrayList<String> names;
+    private ArrayAdapter<String> namesArrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_users);
+
+        logoutButton = (Button)findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        setConversationsList();
+    }
+    private void setConversationsList(){
+        currentUserId = ParseUser.getCurrentUser().getObjectId();
+        names = new ArrayList<String>();
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereNotEqualTo("objectId", currentUserId);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> parseUsers, ParseException e) {
+                if (e == null){
+                    for (int i = 0; i < parseUsers.size(); i++){
+                        names.add(parseUsers.get(i).getUsername().toString());
+                    }
+                    usersListView = (ListView)findViewById(R.id.usersListView);
+                    namesArrayAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.user_list_item, names);
+                    usersListView.setAdapter(namesArrayAdapter);
+                    usersListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            Toast.makeText(getApplicationContext(),"CLICKED USER",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }else{
+                    Toast.makeText(getApplication(), "Error loading User List", Toast.LENGTH_LONG);
+                }
+            }
+        });
+
+
+
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/unknottedb/messenger/LoginActivity.java
@@ -0,0 +1,90 @@
+package com.unknottedb.messenger;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+
+public class LoginActivity extends Activity {
+    private Button signUpButton;
+    private Button loginButton;
+    private EditText usernameField;
+    private EditText passwordField;
+    private String username;
+    private String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Parse.initialize(this, "CUSvRfGOkKbtqRCUvyeWJueCosdm8J27OZh92blz", "UV0Upul7lieWhd1fgvVZAoFeEMCVVO3LlTlKHd8h");
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            Intent i = new Intent(getApplicationContext(), ListUsersActivity.class);
+            startActivity(i);
+        }
+
+        setContentView(R.layout.activity_login);
+
+        usernameField = (EditText)findViewById(R.id.loginUsername);
+        passwordField = (EditText)findViewById(R.id.loginPassword);
+        signUpButton = (Button)findViewById(R.id.signupButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Signup", Toast.LENGTH_SHORT).show();
+                username = usernameField.getText().toString();
+                password = passwordField.getText().toString();
+                ParseUser user = new ParseUser();
+                user.setUsername(username);
+                user.setPassword(password);
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(com.parse.ParseException e) {
+                        if (e == null){
+                            Intent i = new Intent(getApplicationContext(), ListUsersActivity.class);
+                            startActivity(i);
+                        }else {
+                            Toast.makeText(getApplicationContext(), R.string.signup_error, Toast.LENGTH_SHORT ).show();
+                        }
+                    }
+                });
+            }
+        });
+        loginButton = (Button)findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Login", Toast.LENGTH_SHORT).show();
+                username = usernameField.getText().toString();
+                password = passwordField.getText().toString();
+                ParseUser.logInInBackground(username, password, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser parseUser, com.parse.ParseException e) {
+                        if (parseUser != null){
+                            Intent i = new Intent(getApplicationContext(), ListUsersActivity.class);
+                            startActivity(i);
+                        }else{
+                            Toast.makeText(getApplicationContext(),R.string.login_error_password, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+
+
+
+
+
+    }
+
+}
